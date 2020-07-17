e9686d82dc17eadfa4ba2e09a3bb25935635530b
haley <hs.huang@faceheart.com>
2020-07-17 19:08:29 +0800

    ;;;

--- service-worker.js
+++ service-worker.js
@@ -61,46 +61,64 @@ workbox.precaching.precacheAndRoute([{
 
 
 
-// const CACHE = "pwabuilder-page";
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = 'offline';
+// Customize this with a different URL if needed.
+const OFFLINE_URL = 'offline.html';
 
-// // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-// const offlineFallbackPage = "offline.html";
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    // Setting {cache: 'reload'} in the new request will ensure that the response
+    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
+    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
+  })());
+});
 
-// self.addEventListener("message", (event) => {
-//     if (event.data && event.data.type === "SKIP_WAITING") {
-//         self.skipWaiting();
-//     }
-// });
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    // Enable navigation preload if it's supported.
+    // See https://developers.google.com/web/updates/2017/02/navigation-preload
+    if ('navigationPreload' in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
+  })());
 
-// self.addEventListener('install', async (event) => {
-//     event.waitUntil(
-//         caches.open(CACHE)
-//         .then((cache) => cache.add(offlineFallbackPage))
-//     );
-// });
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
+});
 
-// if (workbox.navigationPreload.isSupported()) {
-//     workbox.navigationPreload.enable();
-// }
+self.addEventListener('fetch', (event) => {
+  // We only want to call event.respondWith() if this is a navigation request
+  // for an HTML page.
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        // First, try to use the navigation preload response if it's supported.
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
 
-// self.addEventListener('fetch', (event) => {
-//     if (event.request.mode === 'navigate') {
-//         event.respondWith((async () => {
-//             try {
-//                 const preloadResp = await event.preloadResponse;
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+        // catch is only triggered if an exception is thrown, which is likely
+        // due to a network error.
+        // If fetch() returns a valid HTTP response with a response code in
+        // the 4xx or 5xx range, the catch() will NOT be called.
+        console.log('Fetch failed; returning offline page instead.', error);
 
-//                 if (preloadResp) {
-//                     return preloadResp;
-//                 }
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(OFFLINE_URL);
+        return cachedResponse;
+      }
+    })());
+  }
 
-//                 const networkResp = await fetch(event.request);
-//                 return networkResp;
-//             } catch (error) {
-
-//                 const cache = await caches.open(CACHE);
-//                 const cachedResp = await cache.match(offlineFallbackPage);
-//                 return cachedResp;
-//             }
-//         })());
-//     }
-// });
+  // If our if() condition is false, then this fetch handler won't intercept the
+  // request. If there are any other fetch handlers registered, they will get a
+  // chance to call event.respondWith(). If no fetch handlers call
+  // event.respondWith(), the request will be handled by the browser as if there
+  // were no service worker involvement.
+});
