79f87e55afcc3f43dd9d34a4b9ffb91b8436cf7f
haley <hs.huang@faceheart.com>
2020-08-14 11:06:42 +0800

    ;;;

---
+++ images/service-worker.js
@@ -0,0 +1,124 @@
+'use strict';
+
+console.log('Hello from sw.js');
+
+
+
+// 引用workbox build
+importScripts('./../js/workbox-sw');
+
+
+workbox.setConfig({
+    debug: true
+});
+
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+    console.log(`Boo! Workbox didn't load 😬`);
+}
+
+
+
+
+// 使用cache功能
+// 存任何的.js
+workbox.routing.registerRoute(
+    new RegExp('.*\.js'),
+    new workbox.strategies.CacheFirst({})
+);
+
+// 存任何的css
+workbox.routing.registerRoute(
+    new RegExp('.*\.css'),
+    new workbox.strategies.CacheFirst({
+        cacheName: 'css-cache',
+        cacheableResponse: {
+            statuses: [0, 200]
+        }
+    })
+);
+
+// 存任何的images
+workbox.routing.registerRoute(
+    new RegExp('.*\.(?:png|jpg|jpeg|svg|gif)'),
+    new workbox.strategies.CacheFirst({
+        cacheName: 'image-cache',
+        cacheableResponse: {
+            statuses: [0, 200]
+        }
+    })
+);
+
+// precache放routing之後
+// workbox.precaching.precacheAndRoute([]);
+
+workbox.precaching.precacheAndRoute([{
+    url: 'index.html',
+    revision: '123456'
+}]);
+
+
+
+
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = 'offline';
+// Customize this with a different URL if needed.
+const OFFLINE_URL = 'offline.html';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    // Setting {cache: 'reload'} in the new request will ensure that the response
+    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
+    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
+  })());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    // Enable navigation preload if it's supported.
+    // See https://developers.google.com/web/updates/2017/02/navigation-preload
+    if ('navigationPreload' in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
+  })());
+
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // We only want to call event.respondWith() if this is a navigation request
+  // for an HTML page.
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        // First, try to use the navigation preload response if it's supported.
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
+
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+        // catch is only triggered if an exception is thrown, which is likely
+        // due to a network error.
+        // If fetch() returns a valid HTTP response with a response code in
+        // the 4xx or 5xx range, the catch() will NOT be called.
+        console.log('Fetch failed; returning offline page instead.', error);
+
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(OFFLINE_URL);
+        return cachedResponse;
+      }
+    })());
+  }
+
+  // If our if() condition is false, then this fetch handler won't intercept the
+  // request. If there are any other fetch handlers registered, they will get a
+  // chance to call event.respondWith(). If no fetch handlers call
+  // event.respondWith(), the request will be handled by the browser as if there
+  // were no service worker involvement.
+});
---
+++ js/main.js
@@ -0,0 +1,178 @@
+'use strict';
+var version = "v 1.0";
+
+const getNode = query => document.querySelector(query);
+const getNewCardNode = (node => () => document.importNode(node, true))(getNode('#template'));
+
+
+const writeDataToNode = (node, image, data) => {
+    var _img = node.content.querySelector('#img');
+    var _txt = node.content.querySelector('#txt');
+    var _enroll = node.content.querySelector('#btnenroll');
+    var _modify = node.content.querySelector('#btnmodify');
+    var _btndelete = node.content.querySelector('#btndelete');
+    var _btndetail = node.content.querySelector('#btnDetail');
+
+    _img.src = `data:image/jpg;base64,${image}`;
+    _enroll.onclick = function () {
+
+        var nickname = prompt("請輸入名稱", "");
+        if (nickname != null) {
+            var tmp = '/users.cgi?cmd=enroll&uuid=' + data.uuid + '&username=' + nickname;
+            Send(tmp);
+            _txt.innerHTML = nickname;
+        }
+    };
+
+    _modify.onclick = function () {
+
+        var nickname = prompt("請輸入名稱", "");
+        if (nickname != null) {
+            var tmp = '/users.cgi?cmd=update&pid=' + data.pid + '&username=' + nickname;
+            Send(tmp);
+            Send("/users.cgi?cmd=facesenrolllist");
+        }
+    };
+
+    _btndelete.onclick = function () {
+
+        var answer = confirm('Are you sure you want to delete this?');
+        if (answer) {
+            var tmp = '/users.cgi?cmd=delete&pid=' + data.pid;
+            Send(tmp);
+            Send("/users.cgi?cmd=facesenrolllist");
+        }
+    };
+
+    _btndetail.onclick = function () {
+
+            var tmp = '/users.json?pid=' + data.pid;
+            Send(tmp);
+    };
+
+    if (data.username == "") {
+        _txt.innerText = "unknown";
+    } else {
+        _txt.innerText = data.username;
+    }
+    return node;
+};
+
+
+
+
+
+function Show(cmd, count, data) {
+
+    var container = getNode('div.people');
+    container.innerHTML = "";
+    let newNode;
+    let tmpNode;
+
+    for (var i = 0; i < count; i++) {
+        switch (cmd) {
+            case "/users.cgi?cmd=facesenrolllist":
+
+                tmpNode = getNewCardNode();
+                tmpNode.content.querySelector('#btnenroll').style.visibility = 'hidden';
+                tmpNode.content.querySelector('#btnDetail').style.visibility = 'hidden';
+                newNode = writeDataToNode(tmpNode, data.faces[i].img[0].img, data.faces[i]);
+                break;
+            case "/users.cgi?cmd=faceslivelist":
+
+                if (data.faces[i].img == "" && data.faces[i].username == "") break;
+                tmpNode = getNewCardNode();
+                tmpNode.content.querySelector('#btnmodify').style.visibility = 'hidden';
+                tmpNode.content.querySelector('#btndelete').style.visibility = 'hidden';
+                tmpNode.content.querySelector('#btnDetail').style.visibility = 'hidden';
+                if (data.faces[i].img == "") {
+                    newNode = writeDataToNode(tmpNode, data.faces[i].img, data.faces[i]);
+                } else {
+                    newNode = writeDataToNode(tmpNode, data.faces[i].img, data.faces[i]);
+                }
+                break;
+
+            default:
+
+                break;
+        }
+
+
+        if (newNode)
+            container.prepend(newNode.content);
+    }
+}
+
+//cmd 事件
+$("#enrollfaceslist").click(function () {
+
+    Send("/users.cgi?cmd=facesenrolllist");
+});
+
+$("#livefaceslist").click(function () {
+
+    Send("/users.cgi?cmd=faceslivelist");
+});
+
+$("#modebio").click(function () {
+
+    Send("/sys.cgi?cmd=biomode");
+});
+
+$("#modeenroll").click(function () {
+
+    Send("/sys.cgi?cmd=enrollmode");
+});
+
+
+$(function() {
+    $("#about").on("click",function() {
+        swal({
+            title: "Version",
+            text: version,
+            icon: "success",
+            button: "OK",
+          });
+    });
+  });
+
+
+function Send(cmd) {
+    $.ajax({
+        url: self.location.origin + cmd, //請求的url地址
+        // url:"http://192.168.77.107"+cmd,
+        dataType: "json", //返回格式為json
+        async: true, //請求是否非同步，預設為非同步，這也是ajax重要特性
+        // data: { "id": "value" }, //引數值
+        type: "GET", //請求方式
+        timeout: 5000,
+        beforeSend: function () {
+            //請求前的處理
+        },
+        success: function (req) {
+            //請求成功時處理
+            Analyze(req, cmd);
+        },
+        complete: function () {
+            //請求完成的處理
+        },
+        error: function () {
+            //請求出錯處理
+        }
+    });
+}
+
+function Analyze(data, cmd) {
+
+    document.getElementById("p1").innerHTML = data.msg;
+    switch (cmd) {
+
+        case "/users.cgi?cmd=facesenrolllist":
+        case "/users.cgi?cmd=faceslivelist":
+            Show(cmd, data.faces.length, data);
+            break;
+
+        default:
+            break;
+    }
+}
